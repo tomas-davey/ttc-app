d059b5d2aacd32d2d77b8efbe2dc50e99d592b6f
Tomas Davey <tomas.davey@gmail.com>
2023-10-25 16:38:02 +0100

    something..

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
+import FloatyMan from './FloatyMan.png';
 
 function App() {
   return (
-    <div className='container'>
+    <div className='App'>
       <div className="first-page">
+        <div className='grid-container'>
+          <div className='left-column'>
+            Your Idea,
+            <br />
+            Your business,
+            <br />
+            Our Tech
+          </div>
+          <div className='right-column'>
+            <img src={FloatyMan} alt="FloatyMan" className="FloatyMan"/>
+          </div>
+        </div>
       </div> 
-      <div className='second-page'>
+      <div className="second-page">
       </div>
     </div>
   );
