5e97500fc327e989b8e7aff2e7fbaa593e25155c
Tomas Davey <tomas.davey@gmail.com>
2023-10-25 17:50:18 +0100

    Almost first page lol

--- src/App.js
+++ src/App.js
@@ -5,21 +5,31 @@ function App() {
   return (
     <div className='App'>
       <div className="first-page">
+        <div className='navbar'>
+          <span className="company-name">TTC</span>
+          <a href="#home">Home</a>
+          <a href="#services">Services</a>
+          <a href="#contact">Contact Us</a>
+        </div>
         <div className='grid-container'>
           <div className='left-column'>
-            Your Idea,
-            <br />
-            Your business,
-            <br />
-            Our Tech
+            <div className='title'>
+              Your Idea,
+              <br />
+              Your Business,
+              <br />
+              Our Tech
+            </div>
+            <div className='subtext'>
+              Merging AI, data, and web development to boost businesses from barber shops to restaurants. Elevate with us.
+            </div>
           </div>
           <div className='right-column'>
-            <img src={FloatyMan} alt="FloatyMan" className="FloatyMan"/>
+            <img src={FloatyMan} alt="FloatyMan" className="FloatyMan" />
           </div>
         </div>
-      </div> 
-      <div className="second-page">
       </div>
+      <div className="second-page"></div>
     </div>
   );
 }
