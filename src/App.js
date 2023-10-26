ec8065b11aae30d4a5953963fcc089caecdcf299
tomal214 <thomasalhariri@gmail.com>
2023-10-26 09:51:21 +0100

    has 3d planet in top left -for fun

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+// App.js
+
 import './App.css';
-import SecondPageContent from './secondPage/second_page';  // Import the new component
+import SecondPageContent from './secondPage/second_page';  // Your existing imports
+import Spline from '@splinetool/react-spline';  // Import the Spline component
 
 function App() {
   return (
     <div className='container'>
       <div className="first-page">
+        {/* Wrap the Spline component in a container */}
+        <div className="SplineContainer">
+          <Spline scene="https://prod.spline.design/zqbufJGW6IcE-SPA/scene.splinecode" />
+        </div>
       </div> 
       <div className='second-page'>
-        <SecondPageContent />   {/* Use the new component */}
+        <SecondPageContent />   {/* Your existing second page component */}
       </div>
     </div>
   );
