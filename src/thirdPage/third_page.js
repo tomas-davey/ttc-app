158507cf441ef80a8c18a5da6946476091d4c4b0
tomal214 <thomasalhariri@gmail.com>
2024-02-05 09:54:29 +0000

    same as last commit message

--- src/thirdPage/third_page.js
+++ src/thirdPage/third_page.js
@@ -4,7 +4,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 // import spacemanImage from './Spaceman3.png'; 
 import ScrollVideo from '../animations/scroll_animations';
-import iphoneAnimation from '../animations/newest_animations/the_ones_actually_being_used_that_work_i_think/ttc-iphone_3.mp4';
+import iphoneAnimation from '../animations/newest_animations/using_these/ttc-iphone_3.mp4';
 import FadeTextComponent from '../animations/scroll_fade';
 
 const useStyles = makeStyles((theme) => ({
